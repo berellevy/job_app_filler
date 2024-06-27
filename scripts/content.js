ebb1c8108d73a708d6deb1e7c870833c45e4fba1
berellevy <berellevy@gmail.com>
2024-06-27 12:58:40 -0400

    some updates

--- scripts/content.js
+++ scripts/content.js
@@ -1,19 +1,8 @@
-function get_element(path) {
-  return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-}
 
 
-
-
-
-
-const observer = new MutationObserver(mutations => {
-  mutations.forEach( m => {
-    const elements = get_elements(m.target, FORM_TEXT_INPUT)
-    elements.forEach(registerInput)
-    // console.dir(elements);
-
-  })
+const observer = new MutationObserver((_) => {
+  const elements = get_elements(document, FORM_TEXT_INPUT)
+  elements.forEach(registerInput)
 });
 observer.observe(document.body, {
   childList: true,
