651417f2f58b3f4a939b497629f09ce5ed15b272
berellevy <berellevy@gmail.com>
2024-06-27 12:58:28 -0400

    some updates.

--- scripts/autodiscoverInputs.js
+++ scripts/autodiscoverInputs.js
@@ -1,38 +1,63 @@
 FORM_TEXT_INPUT = `
-  //div[starts-with(@data-automation-id, 'formField-')]
+  .//div[starts-with(@data-automation-id, 'formField-')]
   [.//input[@type='text'] or .//input[@type='password']]
   [not(.//*[@aria-haspopup])]
 `
 
 
 
+const get_answer = ({page, fieldName, type, }) => {
+  return {
+    "Email Address": "berellevy@gmail.com"
+  }[fieldName]
+}
 
 
-function get_element(parent, path) {
+
+const get_element = (parent, path) => {
   return document.evaluate(path, parent, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
 
-function get_elements(parent, path) {
+const get_elements = (parent, path) => {
   const iterator = document.evaluate(path, parent, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null)
   const result = []
   for (let i = 0; i < iterator.snapshotLength; i++) {
     result.push(iterator.snapshotItem(i))
   }
   return result
-
-
 }
 
-
 const registerInput = (node) => {
-  if (!node.hasOwnProperty("jobAppFillerData")) {
-    
-    node.jobAppFillerData = {
-      node: node,
-      get name() {
-        return get_element(this.node, "//label")
+  if (node.hasOwnProperty("jobAppFillerData")) {return}
+
+  const input = {
+    node: node,
+    type: "TextInput",
+    get page() { return get_element(document, ".//h2").innerText },
+    get fieldName() { return get_element(node, ".//label").innerText },
+    get path() { return { page: this.page, type: this.type, fieldName: this.fieldName, } },
+    get inputElement() {return get_element(node, ".//input")},
+    get currentValue() { return this.inputElement.value },
+    get snapshot() { return { ...this.path, "answer": this.currentValue } },
+    get answer() { return get_answer(this.path)},
+    get isFilled() { return this.currentValue === this.answer },
+    fill: function() {
+      if (!this.isFilled) {
+        const inputElement = this.inputElement
+        // inputElement.value = this.answer
+        const keyPressEvent = new KeyboardEvent('keypress', { key: key, bubbles: true, cancelable: true });
+        inputElement.dispatchEvent(keyPressEvent);
+        console.log("fill");
+        // sendKeyPresses(inputElement, "hello")
       }
-    }
-    console.dir(node);
+    },
+
   }
+  node.jobAppFillerData = true
+  const button = document.createElement("button")
+  button.innerHTML = "Fill"
+  button.onclick = () => input.fill()
+  node.appendChild(button)
+
+
 }
