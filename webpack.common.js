12f53896139539133dfc95b1bd57ac6d087cd9ee
berellevy <berellevy@gmail.com>
2024-07-08 17:23:27 -0400

    disable chunks optimization. it doesn't work with web_accessible_resources

--- webpack.common.js
+++ webpack.common.js
@@ -3,18 +3,16 @@ const CopyPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
-
-
 module.exports = {
   entry: {
-    inject: path.resolve("src/inject/inject.tsx"),
-    background: path.resolve("src/background/background.ts"),
-    contentScript: path.resolve("src/contentScript/contentScript.ts"),
+    inject: path.resolve('src/inject/inject.ts'),
+    background: path.resolve('src/background/background.ts'),
+    contentScript: path.resolve('src/contentScript/contentScript.ts'),
   },
   module: {
-    rules:[
+    rules: [
       {
-        use: "ts-loader",
+        use: 'ts-loader',
         test: /\.tsx?$/,
         exclude: /node_modules/,
       },
@@ -25,47 +23,47 @@ module.exports = {
       {
         type: 'asset/resource',
         test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,
-      }
-    ]
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin({
-      cleanStaleWebpackAssets: false
+      cleanStaleWebpackAssets: false,
     }),
     new CopyPlugin({
       patterns: [
         {
           from: path.resolve('src/static'),
-          to: path.resolve("dist")
-        }
-      ]
+          to: path.resolve('dist'),
+        },
+      ],
     }),
-    // ...getHtmlPlugins([
-    //   "popup",
-    //   "options"
-    // ])
+    // ...getHtmlPlugins(['inject']),
   ],
   resolve: {
-    extensions: ['.tsx', '.ts', '.js']
+    extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    filename: "[name].js",
-    path: path.resolve('dist')
+    filename: '[name].js',
+    path: path.resolve('dist'),
   },
-  optimization: {
-    splitChunks: {
-      chunks(chunk) {
-        return chunk.name !== 'contentScript'
-      }
-    }
-  }
+  // optimization: {
+  //   splitChunks: {
+  //     chunks(chunk) {
+  //       // return chunk.name !== 'contentScript'
+  //       return true
+  //     },
+  //   },
+  // },
 }
 
-
 function getHtmlPlugins(chunks) {
-  return chunks.map(chunk => new HtmlWebpackPlugin({
-    title: "React Extension",
-    filename: `${chunk}.html`,
-    chunks: [chunk],
-  }))
+  return chunks.map(
+    (chunk) =>
+      new HtmlWebpackPlugin({
+        title: 'React Extension',
+        filename: `${chunk}.html`,
+        chunks: [chunk],
+      })
+  )
 }
