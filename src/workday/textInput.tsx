ec0eb8072c66d58c0878392d4d71c9b855d54f77
berellevy <berellevy@gmail.com>
2024-07-11 14:55:35 -0400

    build out some more subclass-specific logic

--- src/workday/textInput.tsx
+++ src/workday/textInput.tsx
@@ -1,49 +1,71 @@
 import React from 'react'
-import { getElement, getElements } from '../utils/getElements'
+import { getElement } from '../utils/getElements'
 import '@fontsource/roboto'
-import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
-import { BaseFormInput } from './baseFormInput'
-import { TEXT_INPUT } from './xpaths'
+import { BaseFormInput, getReactProps } from './baseFormInput'
+import * as xpaths from './xpaths'
 
-export class TextInput extends BaseFormInput {
-  static XPATH = TEXT_INPUT
+
+type TextAnswerType = string | null
+
+
+export class TextInput extends BaseFormInput<TextAnswerType> {
+  static XPATH = xpaths.TEXT_INPUT
+  fieldType = "TextInput"
+  
+  public get inputElement(): HTMLInputElement {
+    return getElement(this.element, './/input') as HTMLInputElement
+  }
+
+  listenForChanges() {
+    this.inputElement.addEventListener("input", (e) => {
+      this.triggerReactUpdate()
+    })
+  }
+
+  public currentValue(): TextAnswerType {
+    return this.inputElement.value
+  }
+
+  async answer(): Promise<TextAnswerType> {
+    const res =  await this.fetchAnswer<TextAnswerType>()
+    return res.answer
+  }
+
+  async fill() {
+    const answer = await this.answer()
+    const reactProps = getReactProps(this.inputElement)
+    reactProps.onChange({ target: { value: answer } })
+  }
+}
+
+
+export class PasswordInput extends BaseFormInput<TextAnswerType>{
+  static XPATH = xpaths.PASSWORD_INPUT
+  fieldType = "PasswordInput"
 
   public get inputElement(): HTMLInputElement {
     return getElement(this.element, './/input') as HTMLInputElement
   }
-  public get currentValue(): string | null {
+
+  currentValue(): string | null {
     return this.inputElement.value
   }
 
-  public get reactProps(): any {
-    for (const key in this.element) {
-      if (key.startsWith('__reactProps')) return this.element[key]
-    }
+  listenForChanges() {
+    this.inputElement.addEventListener("input", (e) => {
+      this.triggerReactUpdate()
+    })
+  }
+
+  async answer(): Promise<TextAnswerType> {
+    const res = await this.fetchAnswer<TextAnswerType>()
+    return res.answer
   }
 }
 
-export class PasswordInput extends BaseFormInput {
-  static XPATH = `
-    .//div
-    [starts-with(@data-automation-id, 'formField-')]
-    [.//input[@type='password']]
-    [not(.//*[@aria-haspopup])]
-    `
-  public get inputElement(): HTMLInputElement {
-    return getElement(this.element, './/input') as HTMLInputElement
-  }
-  public get currentValue(): string | null {
-    return this.inputElement.value
-  }
 
-  public get reactProps(): any {
-    for (const key in this.element) {
-      if (key.startsWith('__reactProps')) return this.element[key]
-    }
-  }
-}
 
 export const RegisterInputs = async (node: Node = document) => {
   Promise.all([TextInput.autoDiscover(node), PasswordInput.autoDiscover(node)])
 }
+
