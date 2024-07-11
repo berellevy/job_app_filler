131b58542536377d9355a18eea7660e34b3e047b
berellevy <berellevy@gmail.com>
2024-07-11 14:56:08 -0400

    move password input xpath

--- src/workday/xpaths.ts
+++ src/workday/xpaths.ts
@@ -1,6 +1,13 @@
 export const TEXT_INPUT = `
-.//div
-[starts-with(@data-automation-id, 'formField-')]
-[.//input[@type='text']]
-[not(.//*[@aria-haspopup])]
+  .//div
+  [starts-with(@data-automation-id, 'formField-')]
+  [.//input[@type='text']]
+  [not(.//*[@aria-haspopup])]
+`
+
+export const PASSWORD_INPUT = `
+  .//div
+  [starts-with(@data-automation-id, 'formField-')]
+  [.//input[@type='password']]
+  [not(.//*[@aria-haspopup])]
 `
