2c4dd41aa651f13b544c52c832273368efa97dde
berellevy <berellevy@gmail.com>
2024-07-11 14:52:43 -0400

    add cross context communication server

--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -1,7 +1,22 @@
-import { registerContentScriptEventHandler } from '../utils/storage'
+import { Server } from "../utils/interContextServer";
+import { getAnswer, saveAnswer } from "../utils/storage";
+import { CONTENT_SCRIPT_URL } from "../utils/urls";
+import { FieldSnapshot } from "../workday/baseFormInput";
 
-registerContentScriptEventHandler()
 
+// Regiser server and methods accessible to injected script.
+const server = new Server(CONTENT_SCRIPT_URL)
+server.register('saveAnswer', async (fieldSnapshot: FieldSnapshot) => {
+  return await saveAnswer(fieldSnapshot)
+})
+
+server.register("getAnswer", async (FieldPath) => {
+  const answer =  await getAnswer(FieldPath);
+  return answer
+})
+
+
+// inject script
 function injectScript(filePath: string) {
   const script = document.createElement('script')
   script.src = chrome.runtime.getURL(filePath)
