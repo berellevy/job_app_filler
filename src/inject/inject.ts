ac690577fc4a8eb3cc81fb975f03b170c14a89dc
berellevy <berellevy@gmail.com>
2024-07-11 14:53:13 -0400

    add cross context communication client

--- src/inject/inject.ts
+++ src/inject/inject.ts
@@ -1,4 +1,10 @@
+
+import { Client } from '../utils/interContextServer'
+import { CONTENT_SCRIPT_URL } from '../utils/urls'
 import { RegisterInputs } from '../workday/textInput'
+
+export const client = new Client(CONTENT_SCRIPT_URL)
+
 const observer = new MutationObserver(async (_) => {
   RegisterInputs(document)
 })
