a95cd0b48a193d69380f0a9c1424bf71a5070546
berellevy <berellevy@gmail.com>
2024-07-15 15:32:59 -0400

    add deleteAnswer. clean. reuse getAnswers.

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,6 +1,5 @@
 import { FieldPath, FieldSnapshot } from '../workday/baseFormInput'
 
-
 export interface AnswerData {
   [pageName: string]: {
     [sectionName: string]: {
@@ -12,17 +11,25 @@ export interface AnswerData {
 }
 
 export interface LocalStorage {
-  answers: AnswerData
+  answers?: AnswerData
+}
+
+export const getAnswers = async (): Promise<AnswerData | {}> => {
+  return (await chrome.storage.local.get('answers')).answers || {}
 }
 
 
 /**
  * Checks for each level in the path and creates it if needed.
  */
-export const saveAnswer = async (fieldSnapshot: FieldSnapshot<any>) => {
-  const { page, section, fieldType, fieldName, answer } = fieldSnapshot
-  const existingAnswers =
-    (await chrome.storage.local.get(['answers'])).answers || {}
+export const saveAnswer = async ({
+  page,
+  section,
+  fieldType,
+  fieldName,
+  answer,
+}: FieldSnapshot<any>) => {
+  const existingAnswers = await getAnswers()
 
   if (!(page in existingAnswers)) {
     existingAnswers[page] = {}
@@ -39,30 +46,57 @@ export const saveAnswer = async (fieldSnapshot: FieldSnapshot<any>) => {
 }
 
 
-export const getAnswers = async () => {
-  return await chrome.storage.local.get("answers")
+/**
+ * Delete answer plus any empty paths.
+ * Delete the `fieldName` key and value.
+ * if the containing `fieldType` is empty after deletion, delete it.
+ * if the containing `section` is empty after deletion, delte it, etc.
+ */
+export const deleteAnswer = async ({page, section, fieldType, fieldName}: FieldPath) => {
+  const answers = await getAnswers()
+  if (fieldName in answers?.[page]?.[section]?.[fieldType]) {
+    delete answers[page][section][fieldType][fieldName]
+  }
+  if (Object.keys(answers[page][section][fieldType]).length === 0) {
+    delete answers[page][section][fieldType]
+  }
+  if (Object.keys(answers[page][section]).length === 0) {
+    delete answers[page][section]
+  }
+
+  if (Object.keys(answers[page]).length === 0) {
+    delete answers[page]
+  }
+  const newStorage: LocalStorage = {answers}
+  await chrome.storage.local.set(newStorage)
 }
 
 
 
 /**
- * If there is a full path match in answers storage we 
+ * If there is a full path match in answers storage we
  * consider it to have an aswer and object with an answer key is returned.
  * Even if the value is undefined or null or false.
- * 
+ *
  * If a full path match is not found, return an empty object.
  */
-export type AnswerResponse<AnswerType=any> = {answer: AnswerType} | Record<string, never>
+export type AnswerResponse<AnswerType = any> =
+  | { answer: AnswerType }
+  | Record<string, never>
 
-export const getAnswer = async (path: FieldPath) => {
-  const {page, section, fieldType, fieldName} = path
-  const {answers} = await chrome.storage.local.get("answers")
+export const getAnswer = async ({
+  page,
+  section,
+  fieldType,
+  fieldName,
+}: FieldPath) => {
+  const answers  = await getAnswers()
 
   const hasAnswer = fieldName in (answers?.[page]?.[section]?.[fieldType] || {})
   if (hasAnswer) {
-    return {answer: answers?.[page]?.[section]?.[fieldType]?.[fieldName]}
+    return { answer: answers?.[page]?.[section]?.[fieldType]?.[fieldName] }
   } else {
     return {}
   }
-  return 
+  return
 }
