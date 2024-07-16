3434e5b58e09e39a5d79047723297901fe52ee9e
berellevy <berellevy@gmail.com>
2024-07-16 17:16:24 -0400

    move to workday

---
+++ src/formFields/workday/textInput.ts
@@ -0,0 +1,60 @@
+import { getElement } from '../../utils/getElements'
+import { BaseFormInput, getReactProps } from '../baseFormInput'
+import * as xpaths from './xpaths'
+
+export class TextInput extends BaseFormInput<string | null> {
+  static XPATH = xpaths.TEXT_INPUT
+  fieldType = 'TextInput'
+
+  inputElement(): HTMLInputElement {
+    return getElement(this.element, './/input') as HTMLInputElement
+  }
+
+  listenForChanges() {
+    this.inputElement().addEventListener('input', (e) => {
+      this.triggerReactUpdate()
+    })
+  }
+
+  currentValue(): string | null {
+    return this.inputElement().value
+  }
+
+  async fill() {
+    if (await this.hasAnswer()) {
+      const answer = await this.answer()
+      const reactProps = getReactProps(this.inputElement())
+      reactProps.onChange({ target: { value: answer } })
+    }
+  }
+}
+
+
+
+export class PasswordInput extends BaseFormInput<string | null> {
+  static XPATH = xpaths.PASSWORD_INPUT
+  fieldType = 'PasswordInput'
+
+  inputElement(): HTMLInputElement {
+    return getElement(this.element, './/input') as HTMLInputElement
+  }
+
+  currentValue() {
+    return this.inputElement().value
+  }
+
+  listenForChanges() {
+    this.inputElement().addEventListener('input', (e) => {
+      this.triggerReactUpdate()
+    })
+  }
+
+  async fill() {
+
+    if (await this.hasAnswer()) {
+      const answer = await this.answer()
+      const reactProps = getReactProps(this.inputElement())
+      reactProps.onChange({ target: { value: answer } })
+    }
+  }
+}
