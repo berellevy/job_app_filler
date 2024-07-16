7735d9d9219b437ac44ead476065d097553623ba
berellevy <berellevy@gmail.com>
2024-07-16 17:13:17 -0400

    remove garbage. make primary collor teal. break out more info content

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,34 @@
 import React, { MouseEvent, useEffect, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import SaveIcon from '@mui/icons-material/Save'
-import InfoIcon from '@mui/icons-material/Info'
-import DeleteIcon from '@mui/icons-material/Delete'
 
 import {
-  Card,
-  CardHeader,
-  CardContent,
   Box,
-  IconButton,
   ButtonGroup,
-  createTheme,
   Grid,
   Button,
   Paper,
-  Typography,
   Tooltip,
-  Popper,
-  Fade,
-  TableContainer,
-  TableCell,
-  Table,
-  TableRow,
-  TableBody,
+  createTheme,
+  ThemeProvider,
 } from '@mui/material'
-import { BaseFormInput, SaveStatus } from './workday/baseFormInput'
-import { ThemeProvider } from '@emotion/react'
+import { BaseFormInput} from './formFields/baseFormInput'
 
 import Logo from './components/Logo'
 import { MoreInfoPopper } from './components/MoreInfoPopper'
-import { ConfirmButton } from './components/ConfirmButton'
+import { MoreInfoContent } from './components/MoreInfoContent'
+import { teal } from '@mui/material/colors'
+
 
 const theme = createTheme({
   typography: {
-    // fontSize: 12,
+
+  },
+  palette: {
+    primary: teal,
+    // secondary: 50,
   },
 })
-
-export const SaveButton: React.FC<{
-  onClick?: () => Promise<void>
-}> = ({ onClick }) => {
-  return (
-    <Box>
-      <Paper>
-        <Button size="small" onClick={onClick}>
-          <Typography>Save</Typography>
-        </Button>
-      </Paper>
-    </Box>
-  )
-}
-
-export const FillButton: React.FC<{
-  onClick?: () => void
-  isFilled: boolean
-}> = ({ onClick, isFilled }) => {
-  return (
-    <Box>
-      <Paper>
-        <Button
-          size="small"
-          onClick={onClick}
-          color={isFilled ? 'success' : 'primary'}
-          disabled={isFilled}
-        >
-          <Typography>{isFilled ? 'filled' : 'fill'}</Typography>
-        </Button>
-      </Paper>
-    </Box>
-  )
-}
 
 // TODO: render seperate react app in each subclass and pass the answer type as a generic
 // but for now use any.
@@ -126,68 +84,20 @@ export const App: React.FC<{
           </Grid>
           <Grid item>
             <Paper elevation={4}>
-              <ButtonGroup variant="contained" size="small">
+              <ButtonGroup size="small">
                 <Button onClick={handleFill}>Fill</Button>
                 <Tooltip title="Save current value as answer.">
                   <Button onClick={handleSave}>
-                    <SaveIcon sx={{fontSize: 14}} />
+                    <SaveIcon />
                   </Button>
                 </Tooltip>
                 <MoreInfoPopper title="More Info">
-                  <TableContainer>
-                    <Table size="small">
-                      <TableBody>
-                        <TableRow>
-                          <TableCell align="right" variant="head">
-                            Answer
-                          </TableCell>
-                          <TableCell align="left">
-                            {hasAnswer ? (
-                              <Typography
-                                component="div"
-                                sx={{
-                                  display: 'flex',
-                                  alignItems: 'center',
-                                }}
-                              >
-                                {answer}
-                                <ConfirmButton
-                                  component='IconButton'
-                                  action={handleDeleteAnswer}
-                                  dialogTitle="Are You Sure?"
-                                  buttonContent={<DeleteIcon />}
-                                >
-                                  Are you Sure you want to delete this answer? 
-                                  This action is not reversible.
-                                </ConfirmButton>
-                              </Typography>
-                            ) : (
-                              <Typography
-                                component={'div'}
-                                sx={{
-                                  fontStyle: 'italic',
-                                  display: 'flex',
-                                  alignItems: 'center',
-                                }}
-                              >
-                                No answer
-                                <Tooltip title="Fill in the field and click save to save an answer">
-                                  <InfoIcon
-                                    sx={{ marginLeft: '4px' }}
-                                    fontSize="inherit"
-                                  />
-                                </Tooltip>
-                              </Typography>
-                            )}
-                          </TableCell>
-                        </TableRow>
-                        <TableRow>
-                          <TableCell variant="head">Current</TableCell>
-                          <TableCell>{currentValue}</TableCell>
-                        </TableRow>
-                      </TableBody>
-                    </Table>
-                  </TableContainer>
+                  <MoreInfoContent
+                    answer={answer}
+                    currentValue={currentValue}
+                    handleDeleteAnswer={handleDeleteAnswer}
+                    hasAnswer={hasAnswer}
+                  />
                 </MoreInfoPopper>
               </ButtonGroup>
             </Paper>
