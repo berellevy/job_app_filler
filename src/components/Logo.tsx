6bfd6f22388fc8238d843d3008ee913179a11bca
berellevy <berellevy@gmail.com>
2024-07-16 17:14:22 -0400

    fiddle with sizing

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,32 +1,29 @@
 import { Avatar, Box, Paper, Typography } from '@mui/material'
-import { lime, blueGrey } from '@mui/material/colors'
+import { lime, blueGrey, cyan, green, red, pink, teal } from '@mui/material/colors'
 import React from 'react'
 const variant = "body2"
 const Logo: React.FC = () => {
   return (
-    <Box>
+    <Box >
       <Paper elevation={6} sx={{ borderRadius: '50%' }}>
         <Avatar
-          sx={{ bgcolor: blueGrey['300'], width: '1.75rem', height: '1.75rem' }}
+          sx={{ bgcolor: teal[500], width: '1.7em', height: '1.7em' }}
         >
           <Typography
-            sx={{ fontWeight: '100', fontSize: 12}}
-            color={'black'}
+            sx={{ fontWeight: '100', fontSize: ".65em"}}
             variant={variant}
           >
             j
           </Typography>
           <Typography
-            color={'black'}
             variant={variant}
-            sx={{ fontWeight: '500', fontSize: 14}}
+            sx={{ fontWeight: '500', fontSize: ".75em"}}
           >
             a
           </Typography>
           <Typography
-            color={'black'}
             variant={variant}
-            sx={{ fontWeight: '900', fontSize: 14}}
+            sx={{ fontWeight: '900', fontSize: '.8em'}}
           >
             f
           </Typography>
