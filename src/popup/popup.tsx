ebb48eba18ba5872805c7eb727b8ef7a70446a1d
berellevy <berellevy@gmail.com>
2024-07-25 15:29:01 -0400

    updated popup

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -36,28 +36,26 @@ const App: FC<{}> = () => {
             Elevated autofill for annoying job sites.
           </Typography>
           <Divider sx={{ my: 2 }} />
-          <Typography variant="h6" sx={{ mt: 2, mb: 0 }}>
+
+          <Typography variant="h5" sx={{ mt: 2, mb: 1 }}>
             Feaures
           </Typography>
-          <List dense>
-            <ListItem>
-              <ListItemText>Works. Well.</ListItemText>
-            </ListItem>
-            <ListItem>
-              <ListItemText>Completely open source.</ListItemText>
-            </ListItem>
-            <ListItem>
-              <ListItemText>Your data isn't sent <em>anywhere</em>.</ListItemText>
-            </ListItem>
-          </List>
-          <Button
-            target="_blank"
-            href="https://github.com/berellevy/job_app_filler"
-            startIcon={<GitHubIcon />}
-            variant="outlined"
-          >
-            Contribute
-          </Button>
+          <Typography mb={1}>Works. Well.</Typography>
+          <Typography mb={1}>Completely free, No login required.</Typography>
+          <Typography mb={1}>
+            Your data is stored locally, on your browser and isn't sent{' '}
+            <em>anywhere</em>.
+          </Typography>
+          <Box mt={1}>
+            <Button
+              target="_blank"
+              href="https://github.com/berellevy/job_app_filler"
+              startIcon={<GitHubIcon />}
+              variant="outlined"
+            >
+              Contribute
+            </Button>
+          </Box>
         </Container>
       </Box>
     </ThemeProvider>
