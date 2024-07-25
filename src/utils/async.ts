f4f53c17bf84f92d8ca24585db37a6fca94186f2
berellevy <berellevy@gmail.com>
2024-07-25 15:29:14 -0400

    add awaitable sleep util

---
+++ src/utils/async.ts
@@ -0,0 +1,7 @@
+
+/**
+ * couldn't get timers/promises to work...
+ */
+export const sleep = (timeout: number): Promise<void> => {
+    return new Promise(resolve => setTimeout(resolve, timeout));
+}
