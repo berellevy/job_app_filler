558de1bb0aecb0d18b4b1155d9a890cdabbfa41a
berellevy <berellevy@gmail.com>
2024-08-02 17:11:18 -0400

    formatting

--- src/utils/async.ts
+++ src/utils/async.ts
@@ -1,7 +1,6 @@
-
 /**
  * couldn't get timers/promises to work...
  */
 export const sleep = (timeout: number): Promise<void> => {
-    return new Promise(resolve => setTimeout(resolve, timeout));
+  return new Promise((resolve) => setTimeout(resolve, timeout))
 }
