86a566c068076b83342ae48582e3ab553641dac8
berellevy <berellevy@gmail.com>
2024-08-02 17:06:20 -0400

    add path info and top align titles to support large content

--- src/components/MoreInfoContent.tsx
+++ src/components/MoreInfoContent.tsx
@@ -1,4 +1,8 @@
 import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
   Table,
   TableBody,
   TableCell,
@@ -11,22 +15,66 @@ import React, { FC } from 'react'
 import { ConfirmButton } from './ConfirmButton'
 import DeleteIcon from '@mui/icons-material/Delete'
 import InfoIcon from '@mui/icons-material/Info'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+
+import { FieldPath } from '../formFields/types'
 
 export const MoreInfoContent: FC<{
   hasAnswer: boolean
   answer: any
   handleDeleteAnswer: () => Promise<void>
   currentValue: any
-}> = ({ hasAnswer, answer, handleDeleteAnswer, currentValue }) => {
+  path: FieldPath
+}> = ({ hasAnswer, answer, handleDeleteAnswer, currentValue, path }) => {
+  const { page, section, fieldType, fieldName } = path
   return (
     <TableContainer>
       <Table size="small">
         <TableBody>
           <TableRow>
-            <TableCell align="right" variant="head">
+            <TableCell sx={{ p: 0 }} colSpan={2}>
+              <Accordion>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                  <Typography variant="h6">Path</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Table size="small">
+                    <TableBody>
+                      <TableRow>
+                        <TableCell variant="head" sx={{ verticalAlign: 'top' }}>
+                          Page
+                        </TableCell>
+                        <TableCell>{page}</TableCell>
+                      </TableRow>
+                      <TableRow>
+                        <TableCell variant="head" sx={{ verticalAlign: 'top' }}>
+                          Section
+                        </TableCell>
+                        <TableCell>{section}</TableCell>
+                      </TableRow>
+                      <TableRow>
+                        <TableCell variant="head" sx={{ verticalAlign: 'top' }}>
+                          Question Type
+                        </TableCell>
+                        <TableCell>{fieldType}</TableCell>
+                      </TableRow>
+                      <TableRow>
+                        <TableCell variant="head" sx={{ verticalAlign: 'top' }}>
+                          Question
+                        </TableCell>
+                        <TableCell>{fieldName}</TableCell>
+                      </TableRow>
+                    </TableBody>
+                  </Table>
+                </AccordionDetails>
+              </Accordion>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell sx={{ verticalAlign: 'top' }} variant="head">
               Answer
             </TableCell>
-            <TableCell align="left">
+            <TableCell sx={{ verticalAlign: 'top' }} align="left">
               {hasAnswer ? (
                 <Typography
                   component="div"
@@ -64,8 +112,12 @@ export const MoreInfoContent: FC<{
             </TableCell>
           </TableRow>
           <TableRow>
-            <TableCell variant="head">Current</TableCell>
-            <TableCell>{String(currentValue)}</TableCell>
+            <TableCell sx={{ verticalAlign: 'top' }} variant="head">
+              Current
+            </TableCell>
+            <TableCell>
+              <Typography>{String(currentValue)}</Typography>
+            </TableCell>
           </TableRow>
         </TableBody>
       </Table>
