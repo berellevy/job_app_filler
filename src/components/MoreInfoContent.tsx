f136a1cbcba3d3a8ad6da235c4b34e843a8ec8a3
berellevy <berellevy@gmail.com>
2024-08-07 17:31:41 -0400

    ditch table based display. display paths as breadcrumbs. break out answer display

--- src/components/MoreInfoContent.tsx
+++ src/components/MoreInfoContent.tsx
@@ -1,126 +1,98 @@
 import {
-  Accordion,
-  AccordionDetails,
-  AccordionSummary,
-  Box,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableRow,
+  Breadcrumbs,
+  Chip,
+  Paper,
+  Stack,
+  styled,
   Tooltip,
   Typography,
 } from '@mui/material'
-import React, { FC } from 'react'
-import { ConfirmButton } from './ConfirmButton'
-import DeleteIcon from '@mui/icons-material/Delete'
+import React, { FC} from 'react'
 import InfoIcon from '@mui/icons-material/Info'
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 
-import { FieldPath } from '../formFields/types'
+import { SingleAnswerDisplay } from './SingleAnswerDisplay'
+import { BaseFormInput } from '../formFields/baseFormInput'
+import { BackupAnswerDisplay } from './BackupAnswerDisplay'
+import { AnswerDisplayType, FieldPath } from '../utils/types'
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  color: theme.palette.text.secondary,
+  elevation: 4,
+}))
+
+const answerDisplays = {
+  SingleAnswerDisplay,
+  BackupAnswerDisplay,
+}
+const getAnswerDisplay = (type?: AnswerDisplayType | null): FC<any> => {
+  return answerDisplays[type] || SingleAnswerDisplay
+}
 
 export const MoreInfoContent: FC<{
-  hasAnswer: boolean
+  backend: BaseFormInput<any>
   answer: any
+  answerDisplayType?: AnswerDisplayType
   handleDeleteAnswer: () => Promise<void>
   currentValue: any
   path: FieldPath
-}> = ({ hasAnswer, answer, handleDeleteAnswer, currentValue, path }) => {
-  const { page, section, fieldType, fieldName } = path
+}> = ({
+  answer,
+  handleDeleteAnswer,
+  currentValue,
+  path,
+  answerDisplayType,
+}) => {
+  const AnswerDisplay = getAnswerDisplay(answerDisplayType)
   return (
-    <TableContainer>
-      <Table size="small">
-        <TableBody>
-          <TableRow>
-            <TableCell sx={{ p: 0 }} colSpan={2}>
-              <Accordion>
-                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                  <Typography variant="h6">Path</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                  <Table size="small">
-                    <TableBody>
-                      <TableRow>
-                        <TableCell variant="head" sx={{ verticalAlign: 'top' }}>
-                          Page
-                        </TableCell>
-                        <TableCell>{page}</TableCell>
-                      </TableRow>
-                      <TableRow>
-                        <TableCell variant="head" sx={{ verticalAlign: 'top' }}>
-                          Section
-                        </TableCell>
-                        <TableCell>{section}</TableCell>
-                      </TableRow>
-                      <TableRow>
-                        <TableCell variant="head" sx={{ verticalAlign: 'top' }}>
-                          Question Type
-                        </TableCell>
-                        <TableCell>{fieldType}</TableCell>
-                      </TableRow>
-                      <TableRow>
-                        <TableCell variant="head" sx={{ verticalAlign: 'top' }}>
-                          Question
-                        </TableCell>
-                        <TableCell>{fieldName}</TableCell>
-                      </TableRow>
-                    </TableBody>
-                  </Table>
-                </AccordionDetails>
-              </Accordion>
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell sx={{ verticalAlign: 'top' }} variant="head">
-              Answer
-            </TableCell>
-            <TableCell sx={{ verticalAlign: 'top' }} align="left">
-              {hasAnswer ? (
-                <Typography
-                  component="div"
-                  sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                  }}
-                >
-                  {String(answer)}
-                  <ConfirmButton
-                    component="IconButton"
-                    action={handleDeleteAnswer}
-                    dialogTitle="Are You Sure?"
-                    buttonContent={<DeleteIcon />}
-                  >
-                    Are you Sure you want to delete this answer? This action is
-                    not reversible.
-                  </ConfirmButton>
-                </Typography>
-              ) : (
-                <Typography
-                  component={'div'}
-                  sx={{
-                    fontStyle: 'italic',
-                    display: 'flex',
-                    alignItems: 'center',
-                  }}
-                >
-                  No answer
-                  <Tooltip title="Fill in the field and click save to save an answer">
-                    <InfoIcon sx={{ marginLeft: '4px' }} fontSize="inherit" />
-                  </Tooltip>
-                </Typography>
-              )}
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell sx={{ verticalAlign: 'top' }} variant="head">
-              Current
-            </TableCell>
-            <TableCell>
-              <Typography>{String(currentValue)}</Typography>
-            </TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Stack>
+      <Item>
+        <Typography variant="h6">Question Path:</Typography>
+        <Breadcrumbs separator=">">
+          <Tooltip title="Page">
+            <Chip variant="outlined" label={path.page} />
+          </Tooltip>
+          <Tooltip title="Section">
+            <Chip variant="outlined" label={path.section} />
+          </Tooltip>
+          <Tooltip title="Question Type">
+            <Chip variant="outlined" label={path.fieldType} />
+          </Tooltip>
+          <Tooltip title="Question">
+            <Chip variant="outlined" label={path.fieldName} />
+          </Tooltip>
+        </Breadcrumbs>
+      </Item>
+      <Item>
+        <Typography variant="h6">Current Value</Typography>
+        <Typography>{String(currentValue)}</Typography>
+      </Item>
+      <Item>
+        <Typography variant="h6">Answers</Typography>
+
+        {answer.hasAnswer ? (
+          <AnswerDisplay
+            answer={answer}
+            handleDeleteAnswer={handleDeleteAnswer}
+          />
+        ) : (
+          <Typography
+            component={'div'}
+            sx={{
+              fontStyle: 'italic',
+              display: 'flex',
+              alignItems: 'center',
+            }}
+          >
+            No answer
+            <Tooltip title="Fill in the field and click save to save an answer">
+              <InfoIcon sx={{ marginLeft: '4px' }} fontSize="inherit" />
+            </Tooltip>
+          </Typography>
+        )}
+      </Item>
+    </Stack>
   )
 }
