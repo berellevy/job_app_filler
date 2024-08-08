6777327d6f11cc921d7f3d1a5b7d957154216259
berellevy <berellevy@gmail.com>
2024-08-08 17:21:06 -0400

    add abillity to move open state and anchor  up

--- src/components/MoreInfoPopper.tsx
+++ src/components/MoreInfoPopper.tsx
@@ -9,15 +9,17 @@ import {
   Paper,
   Popper,
 } from '@mui/material'
-import React, { ReactNode, useState, MouseEvent } from 'react'
+import React, { ReactNode, useState, MouseEvent, MutableRefObject } from 'react'
 import MoreVertIcon from '@mui/icons-material/MoreVert'
 import CloseIcon from '@mui/icons-material/Close'
 
 export const MoreInfoPopper: React.FC<{
   children: ReactNode
   title: string
-}> = ({ children, title }) => {
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  anchor: HTMLElement
+  popperState?: [null | HTMLElement, React.Dispatch<React.SetStateAction<HTMLElement>>]
+}> = ({ children, title, popperState, anchor }) => {
+  const [anchorEl, setAnchorEl] = popperState || useState<null | HTMLElement>(null)
   const isOpen = Boolean(anchorEl)
   const id = isOpen ? `more-info-popper` : undefined
 
@@ -31,7 +33,7 @@ export const MoreInfoPopper: React.FC<{
    */
   const handleMoreInfoClick = (e: MouseEvent<HTMLElement>) => {
     if (!isOpen) {
-      setAnchorEl(e.currentTarget)
+      setAnchorEl(anchor || e.currentTarget)
     } else if (e.currentTarget.contains(e.target as Node)) {
       closePopper()
     }
@@ -51,7 +53,7 @@ export const MoreInfoPopper: React.FC<{
           <Fade {...TransitionProps} timeout={350}>
             <Box mx={1}>
               <Paper elevation={8}>
-                <Box sx={{ maxWidth: 'calc(50vw)', maxHeight: 500, overflow: 'scroll' }}>
+                <Box sx={{ maxWidth: 'calc(50vw)', maxHeight: 750, overflow: 'scroll' }}>
                   <Card>
                     <CardHeader
                       sx={{ padding: 1 }}
