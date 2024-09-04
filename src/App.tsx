3f05c8169e1ac4d46111f4a11932127062ccc79c
berellevy <berellevy@gmail.com>
2024-09-04 10:34:27 -0400

    implement context. implement advanced answer matching

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { Children, cloneElement, FC, ReactElement, ReactNode, useContext, useEffect, useRef, useState } from 'react'
 import { createRoot } from 'react-dom/client'
-import SaveIcon from '@mui/icons-material/Save'
-import AutoFixHighIcon from '@mui/icons-material/AutoFixHigh'
+
 
 import {
   Box,
@@ -11,8 +10,8 @@ import {
   Paper,
   Tooltip,
   ThemeProvider,
-  Zoom,
-  Fade,
+  GlobalStyles,
+  Portal,
 } from '@mui/material'
 import { BaseFormInput } from './formFields/baseFormInput'
 
@@ -20,84 +19,32 @@ import Logo from './components/Logo'
 import { MoreInfoPopper } from './components/MoreInfoPopper'
 import { MoreInfoContent } from './components/MoreInfoContent'
 import { theme } from './utils/react'
-import ErrorIcon from '@mui/icons-material/Error'
 import { ButtonSuccessBadge } from './components/ButtonSuccessBadge'
-import { Answer, FieldPath } from './utils/types'
+import { useAppContext, ContextProvider, } from './AppContext'
+import { AutoFixHighIcon, SaveIcon } from './utils/icons'
 
-export type LocalAnswer = [string, Boolean]
-export type LocalAnswerState = [LocalAnswer[], React.Dispatch<React.SetStateAction<LocalAnswer[]>>]
 
 // TODO: render seperate react app in each subclass and pass the answer type as a generic
 // but for now use any.
-export const App: React.FC<{
-  backend: BaseFormInput<any>
-}> = ({ backend }) => {
-  const [answer, setAnswer] = useState<Answer>({
-    answer: null,
-    hasAnswer: false,
-    path: { page: null, section: null, fieldName: null, fieldType: null },
-  })
-  const [currentValue, setCurrentValue] = useState<any>(null)
-  const [error, setError] = useState<string | null>(null)
-  const [isFilled, setIsFilled] = useState<boolean>(false)
-  const [fillButtonDisabled, setFillButtonDisabled] = useState<boolean>(false)
-
-  const buttonGroupRef = useRef(null)
-  const moreInfoPopperState = useState<null | HTMLElement>(null)
-  const setPopperAnchorEl = moreInfoPopperState[1]
-  const localAnswerState = useState<LocalAnswer[]>([])
-  const setLocalAnswerState = localAnswerState[1]
 
 
-  const refresh = async () => {
-    const answer = await backend.answer()
-    setAnswer(answer)
-    setCurrentValue(backend.currentValue())
-    setIsFilled(await backend.isFilled(answer))
-    setError(backend.error)
-  }
+const Main: FC = () => {
+  const appContext = useAppContext()
+  const {
+    backend,
+    fillButtonState,
+    isFilled,
+    moreInfoPopper,
+    saveButtonClickHandler,
+    editableAnswerState
+  } = appContext
 
-  useEffect(() => {
-    ;(async () => {
-      await refresh()
-      await handleFill()
-    })()
-    backend.element.addEventListener(backend.reactMessageEventId, refresh)
+  const {answers} = editableAnswerState
+  
 
-    return () => {
-      backend.element.removeEventListener(backend.reactMessageEventId, refresh)
-    }
-  }, [])
-
-  const saveAnswer = async (newAnswer?: Answer) => {
-    const result: boolean = await backend.save(newAnswer)
-    if (result) {
-      await refresh()
-    }
-  }
-
-  const handleSave = async (newAnswer?: Answer) => {
-    if (backend.answerDisplayType === 'BackupAnswerDisplay' && answer.answer) {
-      setLocalAnswerState([...answer.answer.map((answer: string) => [answer, false]), ["", true]])
-      setPopperAnchorEl(buttonGroupRef.current)
-    } else {
-      saveAnswer(newAnswer)
-    }
-  }
-
-  const handleFill = async () => {
-    setFillButtonDisabled(true)
-    await backend.fill()
-    await refresh()
-    setFillButtonDisabled(false)
-  }
-
-  const handleDeleteAnswer = async () => {
-    await backend.deleteAnswer()
-    await refresh()
-  }
-
+  
   return (
+
     <ThemeProvider theme={theme}>
       <Box my={'4px'}>
         <Grid container spacing={1} alignItems="center">
@@ -106,59 +53,51 @@ export const App: React.FC<{
           </Grid>
           <Grid item>
             <Paper elevation={4}>
-              <ButtonGroup ref={buttonGroupRef} size="small">
+              <ButtonGroup ref={moreInfoPopper.anchorRef} size="small">
                 <Tooltip title="Autofill" placement="top" arrow>
                   <span>
-                    <ButtonSuccessBadge show={answer.hasAnswer && isFilled}>
+                    <ButtonSuccessBadge show={answers.length > 0 && isFilled}>
                       <Button
-                        onClick={handleFill}
-                        disabled={fillButtonDisabled}
+                        onClick={fillButtonState.onClick}
+                        disabled={fillButtonState.isDisabled}
                       >
                         <AutoFixHighIcon />
                       </Button>
                     </ButtonSuccessBadge>
                   </span>
                 </Tooltip>
-                <ButtonSuccessBadge show={answer.hasAnswer}>
+                <ButtonSuccessBadge show={answers.length > 0}>
                   <Tooltip
                     title="Save current value as answer."
                     placement="top"
                     arrow
                   >
-                    <Button onClick={() => handleSave()}>
+                    <Button onClick={() => saveButtonClickHandler(backend.fieldSnapshot, appContext)}>
                       <SaveIcon />
                     </Button>
                   </Tooltip>
                 </ButtonSuccessBadge>
-                <MoreInfoPopper
-                  anchor={buttonGroupRef.current}
-                  popperState={moreInfoPopperState}
-                  title="More Info"
-                >
-                  <MoreInfoContent
-                    localAnswerState={localAnswerState}
-                    backend={backend}
-                    answerDisplayType={backend.answerDisplayType}
-                    answer={answer}
-                    currentValue={currentValue}
-                    handleDeleteAnswer={handleDeleteAnswer}
-                    path={backend.path}
-                    handleSaveAnswer={saveAnswer}
-                  />
+                <MoreInfoPopper title="More Info">
+                  <MoreInfoContent />
                 </MoreInfoPopper>
               </ButtonGroup>
             </Paper>
           </Grid>
-          <Fade in={Boolean(error)}>
-            <Grid item>
-              <Tooltip title={error} TransitionComponent={Zoom}>
-                <ErrorIcon color="error" />
-              </Tooltip>
-            </Grid>
-          </Fade>
         </Grid>
       </Box>
     </ThemeProvider>
+  )
+}
+
+
+export const App: React.FC<{
+  backend: BaseFormInput<any>
+}> = ({ backend }) => {
+  
+  return (
+    <ContextProvider backend={backend}>
+      <Main />
+    </ContextProvider>
   )
 }
 
