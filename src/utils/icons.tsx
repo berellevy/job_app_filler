ea504f2778d1d0a449e8c3adb01aa1f0dbc77e37
berellevy <berellevy@gmail.com>
2024-09-04 11:53:38 -0400

    move icons to single file

---
+++ src/utils/icons.tsx
@@ -0,0 +1,15 @@
+export {default as AddIcon} from '@mui/icons-material/Add';
+export {default as SaveIcon} from '@mui/icons-material/Save'
+export {default as AutoFixHighIcon} from '@mui/icons-material/AutoFixHigh'
+export {default as DeleteIcon} from '@mui/icons-material/Delete'
+export {default as EditIcon} from '@mui/icons-material/Edit'
+export {default as ClearIcon} from '@mui/icons-material/Clear'
+export {default as CheckCircleIcon} from '@mui/icons-material/CheckCircle'
+export {default as InfoIcon} from '@mui/icons-material/Info'
+export {default as MoreVertIcon} from '@mui/icons-material/MoreVert'
+export {default as CloseIcon} from '@mui/icons-material/Close'
+export {default as GitHubIcon} from '@mui/icons-material/GitHub'
+export {default as ArrowRightIcon} from '@mui/icons-material/ArrowRight'
+export {default as ContentCopyIcon} from '@mui/icons-material/ContentCopy'
+export {default as InputIcon} from '@mui/icons-material/Input';
+export  {default as RefreshIcon} from '@mui/icons-material/Refresh';
