2c31e82f6d350b23947a7381abb5c8146df71201
berellevy <berellevy@gmail.com>
2024-09-05 15:24:07 -0400

    clean

--- src/formFields/baseFormInput.tsx
+++ src/formFields/baseFormInput.tsx
@@ -170,7 +170,6 @@ export abstract class BaseFormInput<AnswerType> {
   }
 
   async save(answer: Answer): Promise<Answer> {
-    answer
     const response = await client.send('saveAnswer', answer)
     return response.data
   }
