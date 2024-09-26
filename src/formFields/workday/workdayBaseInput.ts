bc4ade346a6bc0afc381d7f8f5f044044f14eb48
berellevy <berellevy@gmail.com>
2024-09-26 09:45:15 -0400

    start work on accomadation different workday setups.

--- src/formFields/workday/workdayBaseInput.ts
+++ src/formFields/workday/workdayBaseInput.ts
@@ -5,7 +5,8 @@ export abstract class WorkdayBaseInput<
   AnswerType
 > extends BaseFormInput<AnswerType> {
   get sectionLabelXpath(): string {
-    return [
+
+    const primaryXpath = [
       'ancestor::fieldset', // get the nearest ancestor fieldset element
       '/parent::div', // get parent div element of above fieldset element
       `[.//div[@job-app-filler='${this.uuid}']]`, // the above div/fieldset element must contain this forminput's main element.
@@ -13,7 +14,22 @@ export abstract class WorkdayBaseInput<
       '//h4', // get the h4 child of the above div/fieldset. 
       // repeating field sections are the only ones that have an h4 afaik.
     ].join('')
+
+    /**
+     * archgroup workday is different.
+     * not finishing the fix just yet.
+     */
+    const secondaryXpath = [ 
+      'ancestor::div[2]',
+      `[.//div[@job-app-filler='${this.uuid}']]`, // the above div/fieldset element must contain this forminput's main element.
+      "[1]",
+      "//h4"
+    ].join("")
+
+    return `(${primaryXpath})`
   }
+
+
   get sectionLabelElement(): HTMLElement {
     return getElement(this.element, this.sectionLabelXpath)
   }
