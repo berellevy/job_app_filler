a7742870b4b20413171ff508c162cda4cdc45360
berellevy <berellevy@gmail.com>
2024-10-06 16:11:17 -0400

    upgrade save button behavior

--- src/hooks/saveButtonClickHandlers.ts
+++ src/hooks/saveButtonClickHandlers.ts
@@ -1,13 +1,26 @@
-import { AppContextType } from "../AppContext";
-import { Answer } from "../utils/types";
+import { AppContextType } from '../AppContext'
+import { client } from '../inject/client'
+import { Answer } from '../utils/types'
 
 export interface SaveButtonClickHndler {
   (newAnswer: Answer, context: AppContextType): void | Promise<void>
 }
 
+const basic: SaveButtonClickHndler = async (newAnswer, context) => {
+  const resp = await client.send('addAnswer', newAnswer)
+  if (resp.ok) {
+    await context.init()
+  }
+}
+
+const withNotice: SaveButtonClickHndler = async (newAnswer, context) => {
+  const { moreInfoPopper } = context
+  moreInfoPopper.open()
+}
+
 const simpleText: SaveButtonClickHndler = async (newAnswer, context) => {
-  const {editableAnswerState, backend, moreInfoPopper} = context
-  
+  const { editableAnswerState, backend, moreInfoPopper } = context
+
   if (editableAnswerState.answers.length === 0) {
     await backend.save(newAnswer)
     await editableAnswerState.init()
@@ -17,14 +30,13 @@ const simpleText: SaveButtonClickHndler = async (newAnswer, context) => {
   }
 }
 
-
 const backupAnswerList: SaveButtonClickHndler = async (newAnswer, context) => {
-  const {editableAnswerState, backend, moreInfoPopper} = context
-  const {answers, init} = editableAnswerState
+  const { editableAnswerState, backend, moreInfoPopper } = context
+  const { answers, init } = editableAnswerState
   if (answers.length === 0) {
     await backend.save(newAnswer)
     await init()
-  } else if (answers[0].originalAnswer.matchType === "exact") {
+  } else if (answers[0].originalAnswer.matchType === 'exact') {
     // make a popup that says to add a new answer
   }
 }
@@ -32,4 +44,6 @@ const backupAnswerList: SaveButtonClickHndler = async (newAnswer, context) => {
 export const saveButtonClickHandlers = {
   simpleText,
   backupAnswerList,
+  basic,
+  withNotice,
 }
