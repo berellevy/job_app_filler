4dcee1701780ff49b5234c6ea28376c0184753c0
berellevy <berellevy@gmail.com>
2024-10-06 16:12:15 -0400

    upgrade answer storage to 1010

--- src/hooks/useEditableAnswerState.ts
+++ src/hooks/useEditableAnswerState.ts
@@ -1,13 +1,13 @@
-import { Dispatch, FC, SetStateAction, useEffect, useState } from 'react'
+import { useState } from 'react'
 import { BaseFormInput } from '../formFields/baseFormInput'
 import { Answer, FieldPath } from '../utils/types'
-import * as comparePath from '../utils/storage/comparePath'
 import { sleep } from '../utils/async'
+import { client } from '../inject/client'
 
 export type EditableAnswerState = {
   answers: EditableAnswer[]
   initAnswer?: (
-    originalAnswer: Answer,
+    originalAnswer: Answer & { id?: number | string },
     isNew?: boolean,
     editable?: boolean
   ) => EditableAnswer
@@ -31,12 +31,13 @@ export type EditableAnswer = {
   editable: boolean
   isNew: boolean
   error?: string
+  id: number
 }
 
-
+let newAnswerKeyCounter = -1
 
 export const useEditableAnswerState = (
-  backend: BaseFormInput<any>,
+  backend: BaseFormInput<any>
 ): EditableAnswerState => {
   const [answers, setAnswers] = useState<EditableAnswer[]>([])
   const [editedPathMatchesExistingPath, setEditedPathMatchesExistingPath] =
@@ -48,7 +49,13 @@ export const useEditableAnswerState = (
     timeout: number = 1000
   ) => {
     setAnswers((answers) => {
-      answers[id].error = errorMessage
+      answers.find((a) => a.id === id).error = errorMessage
+      return answers.map((answer) => {
+        if (answer.id === id) {
+          answer.error = errorMessage
+        }
+        return answer
+      })
       return structuredClone(answers)
     })
     if (Boolean(errorMessage)) {
@@ -56,14 +63,14 @@ export const useEditableAnswerState = (
     }
   }
 
-
-
   const initAnswer: EditableAnswerState['initAnswer'] = (
     originalAnswer,
     isNew = false,
     editable = false
   ) => {
+    const id = isNew ? newAnswerKeyCounter-- : originalAnswer.id
     return {
+      id,
       originalAnswer,
       editedAnswer: {
         value: backend.answerValue.init(originalAnswer.answer),
@@ -91,20 +98,43 @@ export const useEditableAnswerState = (
 
   const setEditable: EditableAnswerState['setEditable'] = (id, editable) => {
     setAnswers((answers) => {
-      answers[id].editable = editable
-      return answers.slice()
+      return answers.map((answer) => {
+        if (answer.id === id) {
+          answer.editable = editable
+        }
+        return answer
+      })
+      // answers[id].editable = editable
+      // return answers.slice()
     })
   }
 
+  /**
+   * delete if answer is new
+   * re-init if answer is existing
+   */
   const cancelEdit: EditableAnswerState['cancelEdit'] = (id) => {
     setAnswers((answers) => {
-      const answer = answers[id]
-      if (answer.isNew) {
-        delete answers[id]
-      } else {
-        answers[id] = initAnswer(answers[id].originalAnswer)
-      }
-      return answers.slice().filter(Boolean)
+      return answers
+        .map((answer) => {
+          if (answer.id === id) {
+            if (answer.isNew) {
+              return null
+            } else {
+              return initAnswer(answer.originalAnswer)
+            }
+          }
+          return answer
+        })
+        .filter(Boolean)
+
+      // const answer = answers[id]
+      // if (answer.isNew) {
+      //   delete answers[id]
+      // } else {
+      //   answers[id] = initAnswer(answers[id].originalAnswer)
+      // }
+      // return answers.slice().filter(Boolean)
     })
   }
 
@@ -114,10 +144,16 @@ export const useEditableAnswerState = (
     value
   ) => {
     setAnswers((answers) => {
-      answers[id].editedAnswer.path[key] = value
-      return answers.slice()
+      return answers.map((answer) => {
+        if (answer.id === id) {
+          answer.editedAnswer.path[key] = value
+        }
+        return answer
+      })
+      // answers[id].editedAnswer.path[key] = value
+      // return answers.slice()
     })
-    setEditedPathMatchesExistingPath(checkEditedPathMatchesExistingPath(id))
+    // setEditedPathMatchesExistingPath(checkEditedPathMatchesExistingPath(id))
   }
 
   const setEditedValue: EditableAnswerState['setEditedValue'] = (
@@ -125,71 +161,92 @@ export const useEditableAnswerState = (
     newValue
   ) => {
     setAnswers((answers) => {
-      answers[id].editedAnswer.value = newValue
-      return answers.slice()
+      return answers.map((answer) => {
+        if (answer.id === id) {
+          answer.editedAnswer.value = newValue
+        }
+        return answer
+      })
+      // answers[id].editedAnswer.value = newValue
+      // return answers.slice()
     })
   }
 
   const saveAnswer: EditableAnswerState['saveAnswer'] = async (id) => {
-    const { originalAnswer, editedAnswer, isNew } = answers[id]
-    const pathChanged =
-      !comparePath.exact(editedAnswer.path, originalAnswer.path) && !isNew
-    const existingAnswers = await backend.answer(editedAnswer.path)
-    const pathConflict = existingAnswers?.[0]?.matchType === 'exact'
-
-    if (pathChanged && pathConflict) {
-      tempErrorMessage(id, "Can't overwrite an existing path.", 2000)
-      return
-    } else if (isNew && pathConflict) {
-      tempErrorMessage(id, "Can't overwrite an existing path.", 2000)
-      return
-    } else if (pathChanged) {
-      const answerForSave = {
-        answer: backend.answerValue.prepForSave(editedAnswer.value),
-        path: editedAnswer.path,
-      }
-      const savedAnswer = await backend.save(answerForSave)
-      await backend.deleteAnswer(originalAnswer.path)
+    const { editedAnswer, isNew } = answers.find((a) => a.id === id)
+    const answerForSave = {
+      answer: backend.answerValue.prepForSave(editedAnswer.value),
+      path: editedAnswer.path,
+    }
+    const method = isNew ? 'addAnswer' : 'updateAnswer'
+    const answer = isNew ? { ...answerForSave } : { ...answerForSave, id }
+    const resp = await client.send(method, answer)
+    if (resp.ok) {
+      const newAnswer = initAnswer(resp.data)
       setAnswers((answers) => {
-        answers[id] = initAnswer(savedAnswer)
-        return structuredClone(answers)
-      })
-    } else if (!pathChanged) {
-      const answerForSave = {
-        answer: backend.answerValue.prepForSave(editedAnswer.value),
-        path: editedAnswer.path,
-      }
-      const savedAnswer = await backend.save(answerForSave)
-
-      setAnswers((answers) => {
-        answers[id] = initAnswer(savedAnswer)
-        return structuredClone(answers)
+        let updatedAnswers = [...answers]
+        const existingIndex = answers.findIndex((answer) => answer.id === newAnswer.id)
+        if (existingIndex === -1) {
+          updatedAnswers.push(newAnswer)
+        } else {
+          updatedAnswers[existingIndex] = newAnswer
+        }
+        if (isNew) {
+          updatedAnswers = updatedAnswers.filter((a) => a.id !== id)
+        }
+        return updatedAnswers
       })
     }
+    // const pathChanged =
+    //   !comparePath.exact(editedAnswer.path, originalAnswer.path) && !isNew
+
+    // if (pathChanged) {
+    //   const answerForSave = {
+    //     answer: backend.answerValue.prepForSave(editedAnswer.value),
+    //     path: editedAnswer.path,
+    //   }
+    //   const savedAnswer = await backend.save(answerForSave)
+    //   await backend.deleteAnswer(originalAnswer.id)
+    //   setAnswers((answers) => {
+    //     answers[id] = initAnswer(savedAnswer)
+    //     return structuredClone(answers)
+    //   })
+    // } else if (!pathChanged) {
+    //   const answerForSave = {
+    //     answer: backend.answerValue.prepForSave(editedAnswer.value),
+    //     path: editedAnswer.path,
+    //   }
+    //   const savedAnswer = await backend.save(answerForSave)
+
+    //   setAnswers((answers) => {
+    //     answers[id] = initAnswer(savedAnswer)
+    //     return structuredClone(answers)
+    //   })
+    // }
   }
 
   const deleteAnswer: EditableAnswerState['deleteAnswer'] = async (id) => {
-    const { originalAnswer, isNew } = answers[id]
-    if (!isNew) {
-      await backend.deleteAnswer(originalAnswer.path)
+    const resp = await client.send('deleteAnswer', id)
+    if (resp.ok && resp.data) {
       setAnswers((answers) => {
-        delete answers[id]
-        return answers.slice().filter(Boolean)
+        return answers.filter((answer) => {
+          return !(answer.id === id)
+        })
       })
     }
   }
 
-  const checkEditedPathMatchesExistingPath = (id: number): boolean => {
-    return answers.some((answer, index) => {
-      return (
-        id != index &&
-        comparePath.exact(
-          answers[id].editedAnswer.path,
-          answer.originalAnswer.path
-        )
-      )
-    })
-  }
+  // const checkEditedPathMatchesExistingPath = (id: number): boolean => {
+  //   return answers.some((answer, index) => {
+  //     return (
+  //       id != index &&
+  //       comparePath.exact(
+  //         answers[id].editedAnswer.path,
+  //         answer.originalAnswer.path
+  //       )
+  //     )
+  //   })
+  // }
 
   return {
     answers,
