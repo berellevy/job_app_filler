61c240eef3b4983c20a957a217f62f114dbfb758
berellevy <berellevy@gmail.com>
2024-10-06 16:15:46 -0400

    remove client. put logic in async function. call register inputs once in addition to observer.

--- src/inject/inject.ts
+++ src/inject/inject.ts
@@ -1,17 +1,15 @@
 
 import { RegisterInputs } from '../formFields/workday'
-import { Client } from '../utils/crossContextCommunication/client'
 
-
-
-export const client = new Client(process.env.CONTENT_SCRIPT_URL)
-
-
-
-const observer = new MutationObserver(async (_) => {
+const run = async () => {
+  const observer = new MutationObserver(async (_) => {
+    RegisterInputs(document)
+  })
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+  })
   RegisterInputs(document)
-})
-observer.observe(document.body, {
-  childList: true,
-  subtree: true,
-})
+}
+
+run()
