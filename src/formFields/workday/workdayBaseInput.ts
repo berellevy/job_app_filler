a1267eea6d6dc17ff47d199aa3fe855c00c08bf3
berellevy <berellevy@gmail.com>
2024-10-07 16:20:05 -0400

    some sites structure repeating sections differently

--- src/formFields/workday/workdayBaseInput.ts
+++ src/formFields/workday/workdayBaseInput.ts
@@ -12,21 +12,19 @@ export abstract class WorkdayBaseInput<
       `[.//div[@job-app-filler='${this.uuid}']]`, // the above div/fieldset element must contain this forminput's main element.
       '[1]', // first (nearest) div/fieldset to match this criteria
       '//h4', // get the h4 child of the above div/fieldset. 
-      // repeating field sections are the only ones that have an h4 afaik.
     ].join('')
 
     /**
-     * archgroup workday is different.
-     * not finishing the fix just yet.
+     * different method found occasionally
      */
     const secondaryXpath = [ 
-      'ancestor::div[2]',
+      'ancestor::div[@role="group"][1]', // nearest ancestor div roll group
       `[.//div[@job-app-filler='${this.uuid}']]`, // the above div/fieldset element must contain this forminput's main element.
       "[1]",
-      "//h4"
+      "//h4[@id]" // h4 needs an id attribute because repeating sections will reference that id, but non-repeating sections with h4s won't.
     ].join("")
 
-    return `(${primaryXpath})`
+    return `(${primaryXpath} | ${secondaryXpath})`
   }
 
 
