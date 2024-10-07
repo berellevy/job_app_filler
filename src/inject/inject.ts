b5969e767212c164827ad2f5d3ac44a4b7f34045
berellevy <berellevy@gmail.com>
2024-10-07 18:09:29 -0400

    add greenhouse.io

--- src/inject/inject.ts
+++ src/inject/inject.ts
@@ -1,7 +1,14 @@
-
-import { RegisterInputs } from '../formFields/workday'
+import { RegisterInputs as workday } from '../formFields/workday'
+import { RegisterInputs as greenhouse } from '../formFields/greenhouse'
+const inputSetups = {
+  "myworkdayjobs.com": workday,
+  "myworkdaysite.com": workday,
+  "greenhouse.io": greenhouse
+}
 
 const run = async () => {
+  const domain = window.location.host.split('.').slice(-2).join(".")
+  const RegisterInputs = inputSetups[domain]
   const observer = new MutationObserver(async (_) => {
     RegisterInputs(document)
   })
