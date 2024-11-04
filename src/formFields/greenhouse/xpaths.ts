e5d13f51579068d7a1038db20f7c482e1936e278
berellevy <berellevy@gmail.com>
2024-11-04 10:02:37 -0500

    add single file upload and prevent text field from detecting other field types

--- src/formFields/greenhouse/xpaths.ts
+++ src/formFields/greenhouse/xpaths.ts
@@ -1,4 +1,9 @@
 export const TEXT_FIELD = [
   ".//div[@class='field']",
-  "[//input[@type='text']]"
+  "[(./input[@type='text']) | (./label/input[@type='text']) ]"
+].join("")
+
+export const SINGLE_FILE_UPLOAD = [
+  ".//div[@class='field']",
+  "[.//div[contains(@class, 'drop-zone')]]"
 ].join("")
