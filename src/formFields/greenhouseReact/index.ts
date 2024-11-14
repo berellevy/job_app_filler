11ae7a2fe7525aaeee2c5d4c1719e7b75758d984
berellevy <berellevy@gmail.com>
2024-11-14 11:00:21 -0500

    prevent form registering dropdowns on jobsearch page

--- src/formFields/greenhouseReact/index.ts
+++ src/formFields/greenhouseReact/index.ts
@@ -1,14 +1,23 @@
+import { getElement } from "../../utils/getElements";
 import { Dropdown } from "./Dropdown"
 import { File } from "./File"
 import { TextInput } from "./TextInput"
+/** Not present in job search page */
+
+const applicationContainerElement = getElement(
+  document,
+  `.//div[@class="application--container"]`
+)
 
 export const RegisterInputs = async (node: Node = document) => {
-  const inputs = [
-    TextInput,
-    File,
-    Dropdown,
-  ]
-  Promise.all(
-    inputs.map(i => i.autoDiscover(node))
-  )
+  if (applicationContainerElement) {
+    const inputs = [
+      TextInput,
+      File,
+      Dropdown,
+    ]
+    Promise.all(
+      inputs.map(i => i.autoDiscover(node))
+    )
+  }  
 }
