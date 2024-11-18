f910796b80b9f76fcf851a58ec1773c2f3f89604
berellevy <berellevy@gmail.com>
2024-11-18 14:28:28 -0500

    clean listen for changes

--- src/formFields/workday/TextArea.ts
+++ src/formFields/workday/TextArea.ts
@@ -1,7 +1,7 @@
 import fieldFillerQueue from '../../utils/fieldFillerQueue'
 import { getElement } from '../../utils/getElements'
 
-import { getReactProps } from '../utils'
+import { fillReactTextInput, getReactProps } from '../utils'
 import { WorkdayBaseInput } from './workdayBaseInput'
 import * as xpaths from './xpaths'
 
@@ -10,47 +10,31 @@ export class TextArea extends WorkdayBaseInput<string | null> {
   fieldType = 'TextInput'
   private internalValue: string | null
 
-  inputElement(): HTMLInputElement {
+  get inputElement(): HTMLInputElement {
     return getElement(this.element, './/textarea') as HTMLInputElement
   }
 
-  /**
-   * TODO: explain
-   */
   listenForChanges() {
-    const callback = (e) => {
-      this.triggerReactUpdate()
-      this.element.removeEventListener('input', callback)
-      this.inputElement().removeEventListener('blur', callback)
-      this.internalValue = e.target.value
-      setTimeout(() => this.listenForChanges(), 0)
-    }
-    this.element.addEventListener('input', callback)
-    this.inputElement().addEventListener('blur', callback)
+    this.element.addEventListener('input', () => this.triggerReactUpdate())
+    this.inputElement.addEventListener('blur', () => this.triggerReactUpdate())
   }
 
   currentValue(): string | null {
-    return this.internalValue
+    return this.inputElement.value
   }
 
   public isFilled(current: any, stored: any[]): boolean {
-    return current===stored[0]
+    return current === stored[0]
   }
 
-  /**
-   * TODO: explain
-   */
   async fill(): Promise<void> {
     const answer = await this.answer()
     if (answer.length > 0) {
       await fieldFillerQueue.enqueue(async () => {
-        const reactProps = getReactProps(this.inputElement())
-        reactProps.onBlur({ target: { value: answer[0].answer } })
-        this.internalValue = answer[0].answer
+        fillReactTextInput(this.inputElement, answer[0].answer, {
+          eventName: 'onBlur',
+        })
       })
     }
-    setTimeout(() => {
-      this.triggerReactUpdate()
-    }, 0)
   }
 }
