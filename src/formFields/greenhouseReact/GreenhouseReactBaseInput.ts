bde703101788f3863a50c1d8045ecfd8cffbea89
berellevy <berellevy@gmail.com>
2024-11-18 19:55:29 -0500

    add section logic

--- src/formFields/greenhouseReact/GreenhouseReactBaseInput.ts
+++ src/formFields/greenhouseReact/GreenhouseReactBaseInput.ts
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { BaseFormInput } from '../baseFormInput'
+import { getElement } from '../../utils/getElements'
 
 export abstract class GreenhouseReactBaseInput<
   AnswerType
@@ -9,6 +10,16 @@ export abstract class GreenhouseReactBaseInput<
   }
   abstract get labelElement(): HTMLElement
 
+  sectionElement(): HTMLElement {
+    return getElement(
+      this.element,
+      `ancestor::div[@jaf-section][1]`
+    )
+  }
+
+  get section(): string {
+    return this.sectionElement()?.getAttribute("jaf-section") || ""
+  }
   attachReactApp(app: React.ReactNode, inputContainer: HTMLElement): void {
     const rootElement = document.createElement('div')
     rootElement.classList.add('jaf-widget')
