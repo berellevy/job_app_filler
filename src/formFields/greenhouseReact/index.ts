97572bc245ebbbc1a9001cf9754e1757b5f2466d
berellevy <berellevy@gmail.com>
2024-11-18 19:55:18 -0500

    add section and number input

--- src/formFields/greenhouseReact/index.ts
+++ src/formFields/greenhouseReact/index.ts
@@ -1,12 +1,13 @@
-import { getElement } from "../../utils/getElements";
-import { CheckboxBoolean } from "./CheckboxBoolean";
-import { CheckboxMulti } from "./CheckboxMulti";
-import { Dropdown } from "./Dropdown"
-import { DropdownMultiSearchable } from "./DropdownMultiSearchable";
-import { DropdownSearchable } from "./DropdownSearchable";
-import { File } from "./File"
-import { Textarea } from "./Textarea";
-import { TextInput } from "./TextInput"
+import { getElement } from '../../utils/getElements'
+import { CheckboxBoolean } from './CheckboxBoolean'
+import { CheckboxMulti } from './CheckboxMulti'
+import { DropdownMultiSearchable } from './DropdownMultiSearchable'
+import { DropdownSearchable } from './DropdownSearchable'
+import { File } from './File'
+import { NumberInput } from './NumberInput'
+import { Section } from './Section'
+import { Textarea } from './Textarea'
+import { TextInput } from './TextInput'
 
 /** Not present in job search page */
 const applicationContainerElement = getElement(
@@ -18,17 +19,16 @@ const inputs = [
   TextInput,
   Textarea,
   File,
-  // Dropdown,
   DropdownMultiSearchable,
   DropdownSearchable,
   CheckboxMulti,
   CheckboxBoolean,
+  NumberInput,
 ]
 
 export const RegisterInputs = async (node: Node = document) => {
   if (applicationContainerElement) {
-    Promise.all(
-      inputs.map(i => i.autoDiscover(node))
-    )
-  }  
+    await Section.autoDiscover(node)
+    Promise.all(inputs.map((i) => i.autoDiscover(node)))
+  }
 }
