4aebd422f476d7697fd231e1c50ca8fe0627811a
berellevy <berellevy@gmail.com>
2024-12-03 13:37:30 -0500

    add markdown components

---
+++ src/components/Markdown.tsx
@@ -0,0 +1,29 @@
+import { Button, ButtonTypeMap } from '@mui/material'
+import React, { FC } from 'react'
+import { OpenInNewIcon } from '../utils/icons'
+import MuiMarkdown, { getOverrides, MuiMarkdownProps } from 'mui-markdown'
+
+const MarkdownLink: FC<{ children: string; href: string }> = ({
+  children,
+  href,
+}) => {
+  return (
+    <Button
+      variant="text"
+      sx={{ color: 'inherit' }}
+      target="_blank"
+      href={href}
+      endIcon={<OpenInNewIcon />}
+    >
+      {children}
+    </Button>
+  )
+}
+
+export const Markdown: FC<MuiMarkdownProps> = ({ children }) => {
+  return (
+    <MuiMarkdown overrides={{ ...getOverrides(), a: MarkdownLink }}>
+      {children}
+    </MuiMarkdown>
+  )
+}
