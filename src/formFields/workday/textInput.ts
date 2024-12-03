ea4486bd1837bb5f54b435b5bac5f63bc629911e
berellevy <berellevy@gmail.com>
2024-12-03 13:39:38 -0500

    also break out password component

--- src/formFields/workday/textInput.ts
+++ src/formFields/workday/textInput.ts
@@ -60,32 +60,4 @@ export class TextInput extends WorkdayBaseInput<string | null> {
   }
 }
 
-export class PasswordInput extends WorkdayBaseInput<string | null> {
-  static XPATH = xpaths.PASSWORD_INPUT
-  fieldType = 'PasswordInput'
 
-  get inputElement(): HTMLInputElement {
-    return getElement(this.element, './/input') as HTMLInputElement
-  }
-
-  currentValue() {
-    return this.inputElement.value
-  }
-
-  listenForChanges() {
-    this.inputElement.addEventListener('input', (e) => {
-      this.triggerReactUpdate()
-    })
-  }
-
-  public isFilled(current: any, stored: any[]): boolean {
-    return current === stored[0]
-  }
-
-  async fill() {
-    const answer = await this.answer()
-    if (answer.length > 0) {
-      fillReactTextInput(this.inputElement, answer[0].answer)
-    }
-  }
-}
