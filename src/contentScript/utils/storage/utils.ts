ca2e75eb2731824940a7157cf5bba4c7a247e005
berellevy <berellevy@gmail.com>
2024-12-04 13:53:34 -0500

    remove commented out answers106 code

--- src/contentScript/utils/storage/utils.ts
+++ src/contentScript/utils/storage/utils.ts
@@ -1,14 +1,5 @@
 import { FieldPath } from "../../../shared/utils/types"
 
-
-// export const buildKey = ({
-//   page,
-//   section,
-//   fieldType,
-//   fieldName,
-// }: FieldPath) => {
-//   return `${page}:${section}:${fieldType}:${clean(fieldName)}`
-// }
 export const parseKey = (key: string): FieldPath => {
   // there can be colons in the fieldName.
   const [page, section, fieldType, ...fieldName] = key.split(':')
