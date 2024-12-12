1e7d2fe917988c8b606311a6dba6086bd3e7009e
berellevy <berellevy@gmail.com>
2024-12-12 16:56:55 -0500

    inherit from answerdto

--- src/inject/app/services/DTOs/StringDTO.ts
+++ src/inject/app/services/DTOs/StringDTO.ts
@@ -1,33 +1,5 @@
+import AnswerDTO from "./AnswerDTO"
 import { DataType } from "./types"
 
 
-export default class StringDTO {
-  // dataType: DataType = "string"
-  answer: string
-  id: number
-  matchType: string
-  section: string
-  fieldName: string
-  fieldType: string
-
-  path: {
-    section: string
-    fieldName: string
-    fieldType: string
-  }
-
-  constructor({answer, id, matchType, section, fieldName, fieldType}) {
-    this.answer = answer
-    this.id = id
-    this.matchType = matchType
-    this.section = section
-    this.fieldName = fieldName
-    this.path = {
-      section,
-      fieldName,
-      fieldType,
-    }
-  }
-
-  
-}
+export default class StringDTO extends AnswerDTO {}
