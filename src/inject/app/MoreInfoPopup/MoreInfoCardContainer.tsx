af58917b5d10a7bee79808ae9dea148b43d50264
berellevy <berellevy@gmail.com>
2024-12-16 17:25:04 -0500

    start moving away from this.

--- src/inject/app/MoreInfoPopup/MoreInfoCardContainer.tsx
+++ src/inject/app/MoreInfoPopup/MoreInfoCardContainer.tsx
@@ -15,21 +15,23 @@ import { useAppContext } from '../context/AppContext'
 import { MoreInfoHeader } from './MoreInfoHeader'
 import { FieldNotice, Item } from './components'
 import { AnswersSection } from './AnswerSection'
-import { FieldInfo } from './FieldInfo'
+import { Answers } from './Answers'
 
 const MoreInfoCardContainer: FC = () => {
   const { currentValue, fieldNotice } = useAppContext()
 
   return (
-    <Card>
+    <Card sx={{
+      width: 'calc(45vw)',
+    }}>
       <MoreInfoHeader />
       <CardContent sx={{ padding: 0, paddingBottom: '0px!important' }}>
         <Box
           padding={1}
           sx={{
-            maxWidth: 'calc(45vw)',
             maxHeight: 380,
             overflow: 'scroll',
+            
           }}
         >
           <Stack spacing={2}>
@@ -38,16 +40,12 @@ const MoreInfoCardContainer: FC = () => {
                 <FieldNotice>{fieldNotice}</FieldNotice>
               </Item>
             )}
-
             <Item>
-              <AnswersSection />
+              <Answers />
             </Item>
             <Item>
               <Typography variant="h6">Current Value</Typography>
               <Typography>{String(currentValue)}</Typography>
-            </Item>
-            <Item>
-              <FieldInfo />
             </Item>
           </Stack>
         </Box>
