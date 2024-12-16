a67e961f76d55b8703ff974da504fe2ab660b504
berellevy <berellevy@gmail.com>
2024-12-16 17:24:26 -0500

    convert from card to plain

--- src/inject/app/MoreInfoPopup/MoreInfoHeader.tsx
+++ src/inject/app/MoreInfoPopup/MoreInfoHeader.tsx
@@ -1,8 +1,9 @@
-import { CardHeader, IconButton, CircularProgress, Box } from '@mui/material'
+import { IconButton, CircularProgress, Box, Tooltip, Stack, Typography } from '@mui/material'
 import { startCase } from 'lodash'
 import React, { FC } from 'react'
 import { RefreshIcon, CloseIcon } from '@src/shared/utils/icons'
 import { useAppContext } from '../context/AppContext'
+import ExpandableText from '../components/ExpandableText'
 
 export const MoreInfoHeader: FC = () => {
   const {
@@ -12,7 +13,35 @@ export const MoreInfoHeader: FC = () => {
       handleRefreshButtonClick,
       fieldType,
     },
+    backend
   } = useAppContext()
+
+  const Title = (
+    <Typography variant='h5'>
+      <ExpandableText text={backend.fieldName} maxLength={50} />
+    </Typography>
+  )
+
+  const SubHeader = (
+    <Typography variant='subtitle2'>
+      <Tooltip arrow title="Field Type">
+        <span>
+          {startCase(fieldType)}
+        </span>
+      </Tooltip>
+      {backend.section && <>{" | "}<Tooltip arrow title="Section"><span>{backend.section}</span></Tooltip></>}
+    </Typography>
+  )
+
+  const Text = (
+    <Box pl={1} py={"auto"}>
+      {Title}
+      {SubHeader}
+    </Box>
+  )
+
+
+
 
   const RefreshButton = (
     <IconButton onClick={handleRefreshButtonClick}>
@@ -21,23 +50,26 @@ export const MoreInfoHeader: FC = () => {
   )
 
   const CloseButton = (
-    <IconButton aria-label="close" onClick={close} sx={{ marginLeft: 'auto' }}>
+    <IconButton aria-label="close" onClick={close} >
       <CloseIcon />
     </IconButton>
   )
 
+  const Buttons = (
+    <Stack alignItems={"flex-start"} direction={'row'}>
+
+      {RefreshButton}
+      {CloseButton}
+    </Stack>
+  )
+
+
   return (
-    <Box borderBottom="1px solid #e0e0e0">
-      <CardHeader
-        sx={{ padding: 1}}
-        title={startCase(fieldType)}
-        action={
-          <>
-            {RefreshButton}
-            {CloseButton}
-          </>
-        }
-      />
+    <Box borderBottom="1px solid #e0e0e0" p={1}>
+      <Stack direction={"row"} justifyContent={'space-between'}>
+        {Text}
+        {Buttons}
+      </Stack>
     </Box>
   )
 }
