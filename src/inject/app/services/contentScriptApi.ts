de2923281288315adad6514f5e4d701ef1e4a35f
berellevy <berellevy@gmail.com>
2024-12-18 15:44:01 -0500

    restructure params

--- src/inject/app/services/contentScriptApi.ts
+++ src/inject/app/services/contentScriptApi.ts
@@ -1,17 +1,30 @@
 import { Client } from "@src/shared/utils/crossContextCommunication/client";
 import AnswerDTO from "./DTOs/AnswerDTO";
-import { Answer106, FieldPath } from "@src/shared/utils/types";
+import { Answer106, FieldPath, ResponseBody } from "@src/shared/utils/types";
+
+
 const client = new Client(process.env.CONTENT_SCRIPT_URL)
 
-const addAnswer = async (newAnswer: any, DTOClass: typeof AnswerDTO) => {
-    const resp = await client.send("addAnswer", newAnswer)
+type NewAnswer = { question: string, answer: any, section: string, fieldType: string }
+
+const addAnswer = async (newAnswer: NewAnswer, DTOClass: typeof AnswerDTO) => {
+    const {section, question, answer, fieldType} = newAnswer
+    const resp = await client.send("addAnswer", {
+        answer,
+        path: {
+            fieldType,
+            section, 
+            fieldName: question
+        }
+    })
     if (resp.ok) {
-        resp.data = DTOClass.from106(newAnswer)
+        resp.data = DTOClass.from106(resp.data)
     }
     return resp
 }
 
-const updateAnswer = async (answer: any, DTOClass: typeof AnswerDTO) => {
+
+const updateAnswer = async (answer: any, DTOClass: typeof AnswerDTO): Promise<ResponseBody<AnswerDTO>> => {
     const resp = await client.send("updateAnswer", answer)
     if (resp.ok) {
         resp.data = DTOClass.from106(resp.data)
@@ -19,10 +32,10 @@ const updateAnswer = async (answer: any, DTOClass: typeof AnswerDTO) => {
     return resp
 }
 
-const getAnswers = async (path: FieldPath, DTOClass: typeof AnswerDTO): Promise<any[]> => {
+const getAnswers = async (path: FieldPath, DTOClass: typeof AnswerDTO): Promise<AnswerDTO[]> => {
     const resp = await client.send("getAnswer", path)
     if (!resp.ok) {
-        console.error({resp, path})
+        console.error({ resp, path })
         return []
     }
     return resp.data.map((a: Answer106) => DTOClass.from106(a))
